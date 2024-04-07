4cbddcc52a4ee889314015ec900081d561bb91a4
Manelick <l19070036@huetamo.tecnm.mx>
2024-04-06 21:35:35 -0600

    agrege dropdowns a la navbar

--- resources/views/extraEscolares/alumnos.blade.php
+++ resources/views/extraEscolares/alumnos.blade.php
@@ -8,9 +8,9 @@
 <br>
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="flex justify-center items-center">
+        <div class="pt-5">
         <form action="{{route('extraEscolares.alumnos')}}" method="get">
-                <input type="search" name="search" value="{{$texto}}" class="">
+                <input type="search" name="search" value="{{$texto}}" class="w-full">
                 <button type="submit" class="mt-4 bg-green-900 text-white hover:bg-green-700 inline-flex items-center px-4 py-2 bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-gray-800 transition ease-in-out duration-150">Buscar</button>
         </form>
     </div>
