c8bd3b0210f83f5caf70ff5ad93905e464e0dcb0
Manelick <l19070036@huetamo.tecnm.mx>
2024-04-08 18:28:15 -0600

    agrege los links de extraescolares a un componente

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,72 +21,7 @@
                     <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.*')">
                         {{ __('Chirps') }}
                     </x-nav-link>
-
-                    @can('see-extraescolares')
-                    <div class="hidden sm:flex sm:items-center sm:ms-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-400 bg-gray-800 hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                    <div> Encargados</div>
-        
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-        
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('/extraEscolares/admextraescolares')">
-                                    {{ __('Registrar encargado') }}
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-
-                    <div class="hidden sm:flex sm:items-center sm:ms-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-400 bg-gray-800 hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                    <div>Clubs</div>
-        
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-        
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('agregarclub')">
-                                    {{ __('Registrar club') }}
-                                </x-dropdown-link>
-                                <x-dropdown-link :href="route('club.editar')">
-                                    {{ __('Editar club') }}
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-                    <x-nav-link :href="route('extraEscolares.alumnos')" :active="request()->routeIs('extraEscolares.*')">
-                        {{ __('Alumnos') }}
-                    </x-nav-link>
-                    @endcan
-
-                    @can('see-clubs')
-                    <x-nav-link :href="route('/extraEscolares/paselista')" :active="request()->routeIs('/extraEscolares/paselista')">
-                        {{ __('Listas') }}
-                    </x-nav-link>
-                    @endcan
-
-                    @can('clubs')
-                    <x-nav-link :href="route('extraEscolares.index')" :active="request()->routeIs('extraEscolares.*')">
-                        {{ __('Clubs') }}
-                    </x-nav-link>
-                    @endcan
-
-                   
+                    @include('layouts.partials.links-extraescolares');                   
                     <x-nav-link :href="route('seguimiento.index')" :active="request()->routeIs('seguimiento.*')">
                         {{ __('Egresados y empleadores') }}
                     </x-nav-link>
@@ -148,6 +83,7 @@
             <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.*')">
                 {{ __('Chirps') }}
             </x-responsive-nav-link>
+            @include('layouts.partials.links-extraescolares-responsive'); 
             <x-responsive-nav-link :href="route('seguimiento.index')" :active="request()->routeIs('seguimiento.*')">
                 {{ __('Egresados y empleadores') }}
             </x-responsive-nav-link>
