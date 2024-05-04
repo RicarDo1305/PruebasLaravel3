304e07a19a001a1205a40bdd9c140a5bcfb20793
Manelick <l19070036@huetamo.tecnm.mx>
2024-05-03 20:11:35 -0600

    Arregle la plantilla y comenze con la vista para generar listas especiales de los clubs

--- resources/views/extraEscolares/listaespecial.blade.php
+++ resources/views/extraEscolares/listaespecial.blade.php
@@ -25,7 +25,7 @@
             <th class="px-4 py-2 border border-gray-400">Nombre</th>
             <th class="px-4 py-2 border border-gray-400">Carrera</th>
             <th class="px-4 py-2 border border-gray-400">Semestre</th>
-            <th class="px-4 py-2 border border-gray-400">Asistencias</th>
+            <th class="px-4 py-2 border border-gray-400">Participa</th>
         </tr>
         </thead>
         <tbody>
@@ -35,7 +35,7 @@
             <td class="px-4 py-2 border border-gray-400">{{$alumno->name}}</td>
             <td class="px-4 py-2 border border-gray-400">{{$alumno->carrera}}</td>
             <td class="px-4 py-2 border border-gray-400">{{$alumno->semestre}}</td>
-            <form action="{{route('registro.asistencia')}}" method="GET">
+            <form action="{{route('descarga.especial')}}" method="GET">
             <td class="px-4 py-2 border border-gray-400"><input type="checkbox"  value="{{$alumno->name}}" name="name[]"></td>
         </tr>
         @endforeach
@@ -45,11 +45,9 @@
 </div>
 <div class="py-12">
     <div class="flex flex-col items-center">
-        <button class="ms-3 bg-green-900 text-white hover:bg-green-700 'inline-flex items-center 
-        px-1 py-1 md:px-2 md:py-2 bg-gray-200 border border-transparent rounded-md font-semibold text-xs md:text-xs 
-        text-gray-800 uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 
-        focus:ring-offset-gray-800 transition ease-in-out duration-150'" type="submit">Asistencia</button>
-    </form>
+        <x-primary-button class="mt-4 bg-green-900 text-white hover:bg-green-700">
+            Crear lista
+        </x-primary-button>
     </div>
 </div>
     
