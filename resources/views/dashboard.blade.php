e1564ede19ecb1c4437186dd0a3d58b348579972
Manelick <l19070036@huetamo.tecnm.mx>
2024-05-17 21:06:01 -0600

    agregue la barra de progreso para las horas de los alumnos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,15 @@
             <div class="bg-gray-800 overflow-hidden shadow-sm rounded-md sm:rounded-lg">
                 <div class="p-6 text-gray-100 text-xs md:text-base">
                     {{ __("Welcome") }} {{ Auth::user()->name }}
+                    <br>
+                    @can('clubs')
+                    @php
+                    $horas=Auth::user()->horas;
+                    @endphp
+                     <label class="text-white" for="">Progreso de horas:</label>
+                     <progress max="500" value="{{$horas}}"></progress>
+                     {{$horas}} horas liberadas de 250
+                    @endcan
                 </div>
             </div>
         </div>
